15fdc8f391060e827dfcf251a97d4f773352d9d8
klearliu <clearlynetworking@gmail.com>
2024-03-24 23:26:23 -0600

    final submission

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 8000;
+
+export const mongoURL =
+  "mongodb+srv://klearliu:dbpassword@course-registration.qutqelg.mongodb.net/CourseRegistration?retryWrites=true&w=majority&appName=course-registration";
